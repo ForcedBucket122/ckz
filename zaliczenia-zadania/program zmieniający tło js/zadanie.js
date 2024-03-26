538460e996f49eb33d968b963e583f95c01bee68
tomatotownmasacre <jakub7928@gmail.com>
2024-03-26 10:02:30 +0100

    zadania z js

---
+++ zaliczenia-zadania/program zmieniający tło js/zadanie.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("#test");
+const inputs = form.querySelectorAll("input");
+const r = inputs[0];
+const g = inputs[1];
+const b = inputs[2];
+r.addEventListener("input", () => {
+    r.nextElementSibling.innerText = "Wartość pola: " + r.value
+    document.body.style.backgroundColor = "rgb("+r.value+","+g.value+","+b.value+")";
+});
+
+g.addEventListener("input", () => {
+    g.nextElementSibling.innerText = "Wartość pola: " + g.value
+    document.body.style.backgroundColor = "rgb("+r.value+","+g.value+","+b.value+")";
+});
+b.addEventListener("input", () => {
+    b.nextElementSibling.innerText = "Wartość pola: " + b.value
+    document.body.style.backgroundColor = "rgb("+r.value+","+g.value+","+b.value+")";
+});
