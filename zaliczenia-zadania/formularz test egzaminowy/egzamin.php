5da1c46f92908a1df667b9a3732e0b0ba48d7e11
ForcedBucket122 <dzejkob2112@gmail.com>
2024-05-13 22:25:18 +0200

    do naprawy

--- zaliczenia-zadania/formularz test egzaminowy/egzamin.php
+++ zaliczenia-zadania/formularz test egzaminowy/egzamin.php
@@ -1,30 +1,23 @@
 <?php
-//formularz w pętli
-
     session_start();
     if (!isset($_SESSION['count'])) { 
         $_SESSION['count'] = 0;       
     }
-    if ($_SESSION['count']>9) {
+    if ($_SESSION['count'] > 9) {
         $_SESSION['count'] = 0;
     }
-    if ($_SESSION['count']<0) {
+    if ($_SESSION['count'] < 0) {
         $_SESSION['count'] = 9;
     }
-    $_SESSION['odp']=array(
-        0 => '',
-        1 => '',
-        2 => '',
-        3 => '',
-        4 => '',
-        5 => '',
-        6 => '',
-        7 => '',
-        8 => '',
-        9 => ''
-    );
-    
+    if (!isset($_SESSION['odp'])) {
+        $_SESSION['odp'] = array_fill(0, 10, ''); // Inicjalizacja tablicy odpowiedzi
+    }
+
+    if(isset($_POST['odp'])) {
+        $_SESSION['odp'][$_SESSION['count']] = $_POST['odp']; // Zapisanie odpowiedzi do odpowiedniego indeksu
+    }
 ?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -39,99 +32,60 @@
         <div id="wyspa">
             <form action="przod.php" method="post" id="main">
                 <?php
-                    // $_SESSION['count'];
-                    $con=mysqli_connect('localhost','root','','formularz');
-                    $sprawdz='SELECT treść_pyt FROM pytania;';
-                    $query=mysqli_query($con,$sprawdz);
-                    $zap='SELECT id_pyt,odp1,odp2,odp3,odp4 FROM pytania';
-                    $query1=mysqli_query($con,$zap);
-                    $zap2='SELECT COUNT(id_pyt) FROM odpowiedzi GROUP BY id_pyt';
-                    $query2=mysqli_query($con,$zap2);
-                    $typ=[];
-                    while($row=mysqli_fetch_array($query2)){
-                        array_push($typ,$row['COUNT(id_pyt)']);
+                    $con = mysqli_connect('localhost', 'root', '', 'formularz');
+                    $sprawdz = 'SELECT treść_pyt FROM pytania;';
+                    $query = mysqli_query($con, $sprawdz);
+                    $zap = 'SELECT id_pyt, odp1, odp2, odp3, odp4 FROM pytania';
+                    $query1 = mysqli_query($con, $zap);
+                    $zap2 = 'SELECT COUNT(id_pyt) FROM odpowiedzi GROUP BY id_pyt';
+                    $query2 = mysqli_query($con, $zap2);
+                    $typ = [];
+                    while ($row = mysqli_fetch_array($query2)) {
+                        array_push($typ, $row['COUNT(id_pyt)']);
                     }
-                    $pytanie=[];
-                    $odpowiedzi=[];
-                    $nr=$_SESSION['count']+1;
-                    while($row=mysqli_fetch_array($query)){
-                        $dane='<div id="pytanie">'.$nr.". ".$row['treść_pyt'].'</div>';
-                        array_push($pytanie,$dane); 
-                    }while($row=mysqli_fetch_array($query1)){
-                        if($typ[$row['id_pyt']-1]>1){
-                            $dane= 
-                        '<div id="odpowiedzi">
-                            <div id="input">
-                                <input type="checkbox" name="odp[]" id="odp" value=1><p>A. '.$row['odp1'].'</p>
-                            </div>
-                            <div id="input">
-                                <input type="checkbox" name="odp[]" id="odp" value=2><p>B. '.$row['odp2'].'</p>
-                            </div>
-                            <div id="input">
-                                <input type="checkbox" name="odp[]" id="odp" value=3><p>C. '.$row['odp3'].'</p>
-                            </div>
-                            <div id="input">
-                                <input type="checkbox" name="odp[]" id="odp" value=4><p>D. '.$row['odp4']."</p>
-                            </div>
-                        </div>";
-                        }else{
-                            $dane= 
-                            '<div id="odpowiedzi">
-                                <div id="input">
-                                    <input type="radio" name="odp" id="odp" value=1><p>A. '.$row['odp1'].'</p>
-                                </div>
-                                <div id="input">
-                                    <input type="radio" name="odp" id="odp" value=2><p>B. '.$row['odp2'].'</p>
-                                </div>
-                                <div id="input">
-                                    <input type="radio" name="odp" id="odp" value=3><p>C. '.$row['odp3'].'</p>
-                                </div>
-                                <div id="input">
-                                    <input type="radio" name="odp" id="odp" value=4><p>D. '.$row['odp4']."</p>
-                                </div>
-                            </div>"; 
+                    $pytanie = [];
+                    $odpowiedzi = [];
+                    $nr = $_SESSION['count'] + 1;
+                    while ($row = mysqli_fetch_array($query)) {
+                        $dane = '<div id="pytanie">' . $nr . ". " . $row['treść_pyt'] . '</div>';
+                        array_push($pytanie, $dane);
+                    }
+                    while ($row = mysqli_fetch_array($query1)) {
+                        if ($typ[$row['id_pyt'] - 1] > 1) {
+                            $dane = '<div id="odpowiedzi">
+                                        <div id="input">
+                                            <input type="checkbox" name="odp[]" id="odp1" value="1"' . (in_array('1', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>A. ' . $row['odp1'] . '</p>
+                                        </div>
+                                        <div id="input">
+                                            <input type="checkbox" name="odp[]" id="odp2" value="2"' . (in_array('2', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>B. ' . $row['odp2'] . '</p>
+                                        </div>
+                                        <div id="input">
+                                            <input type="checkbox" name="odp[]" id="odp3" value="3"' . (in_array('3', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>C. ' . $row['odp3'] . '</p>
+                                        </div>
+                                        <div id="input">
+                                            <input type="checkbox" name="odp[]" id="odp4" value="4"' . (in_array('4', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>D. ' . $row['odp4'] . '</p>
+                                        </div>
+                                      </div>';
+                        } else {
+                            $dane = '<div id="odpowiedzi">
+                                        <div id="input">
+                                            <input type="radio" name="odp" id="odp1" value="1"' . (in_array('1', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>A. ' . $row['odp1'] . '</p>
+                                        </div>
+                                        <div id="input">
+                                            <input type="radio" name="odp" id="odp2" value="2"' . (in_array('2', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>B. ' . $row['odp2'] . '</p>
+                                        </div>
+                                        <div id="input">
+                                            <input type="radio" name="odp" id="odp3" value="3"' . (in_array('3', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>C. ' . $row['odp3'] . '</p>
+                                        </div>
+                                        <div id="input">
+                                            <input type="radio" name="odp" id="odp4" value="4"' . (in_array('4', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>D. ' . $row['odp4'] . '</p>
+                                        </div>
+                                    </div>';
                         }
-                        
-                        array_push($odpowiedzi,$dane);
-
-                        // if(isset($_POST['odp']))
-                        // array_push($_SESSION['odp'],$dane);
+                        array_push($odpowiedzi, $dane);
                     }
-                    
-                    
                     echo $pytanie[$_SESSION['count']];
                     echo $odpowiedzi[$_SESSION['count']];
-                    // echo $_SESSION['count'];
-                    
-                    // if($typ[$_SESSION['count']]>1){
-                        
-                    // }else{
-                    //     $_SESSION['odp'][$_SESSION['count']]=$_POST['odp'];
-                    // }
-                    
-                ?>
-                <div id="selectedOptions"></div>
-                <script>
-                    // // Pobranie zawartości diva
-                    // var selectedOptions = document.getElementById('selectedOptions').textContent;
-
-                    // // Wysłanie wartości za pomocą AJAX
-                    // var xhr = new XMLHttpRequest();
-                    // xhr.open('POST', 'egzamin.php', true);
-                    // xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                    // xhr.onload = function() {
-                    //     if (xhr.status === 200) {
-                    //         console.log('Odpowiedź z serwera:', xhr.responseText);
-                    //     } else {
-                    //         console.error('Wystąpił błąd:', xhr.statusText);
-                    //     }
-                    // };
-                    // xhr.send('selectedOptions=' + encodeURIComponent(selectedOptions));
-                </script>
-                <?php
-                    // $selectedOptions = $_POST['selectedOptions'];
-
-                    // echo 'Otrzymano wartości: ' . $selectedOptions;
                 ?>
                 <div id="batony">
                     <div style="order:2">
@@ -151,10 +105,6 @@
                     </div>
                 </div>
             </form>
-            
-                                                                                                        
-                
-            
         </div>
     </div>
 </body>
