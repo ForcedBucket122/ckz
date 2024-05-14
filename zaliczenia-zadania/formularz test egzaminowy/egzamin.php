cb8ae71cd5e652176551aab44cf94186219a6859
ForcedBucket122 <151549270+ForcedBucket122@users.noreply.github.com>
2024-05-14 10:21:21 +0200

    switch case

--- zaliczenia-zadania/formularz test egzaminowy/egzamin.php
+++ zaliczenia-zadania/formularz test egzaminowy/egzamin.php
@@ -10,12 +10,15 @@
         $_SESSION['count'] = 9;
     }
     if (!isset($_SESSION['odp'])) {
-        $_SESSION['odp'] = array_fill(0, 10, ''); // Inicjalizacja tablicy odpowiedzi
+        $_SESSION['odp'] = array_fill(0, 10, ""); // Inicjalizacja tablicy odpowiedzi
     }
 
     if(isset($_POST['odp'])) {
         $_SESSION['odp'][$_SESSION['count']] = $_POST['odp']; // Zapisanie odpowiedzi do odpowiedniego indeksu
     }
+    // $text1 = $_POST['odp1'];
+    // $text2 = $_POST['odp2'];
+    // echo $text1;
 ?>
 
 <!DOCTYPE html>
@@ -50,44 +53,99 @@
                         $dane = '<div id="pytanie">' . $nr . ". " . $row['treść_pyt'] . '</div>';
                         array_push($pytanie, $dane);
                     }
+                    // '1'= strval($_SESSION['count']);
                     while ($row = mysqli_fetch_array($query1)) {
                         if ($typ[$row['id_pyt'] - 1] > 1) {
-                            $dane = '<div id="odpowiedzi">
+                            $dane = '<form action="przod.php" method="post" id="main">
+                                        <div id="odpowiedzi">
                                         <div id="input">
-                                            <input type="checkbox" name="odp[]" id="odp1" value="1"' . (in_array('1', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>A. ' . $row['odp1'] . '</p>
+                                            <input type="checkbox" name="odp[]" id="odp1" value="1"' . (in_array('1', $_SESSION['odp']) ? ' checked' : '') . '><p>A. ' . $row['odp1'] . '</p>
                                         </div>
                                         <div id="input">
-                                            <input type="checkbox" name="odp[]" id="odp2" value="2"' . (in_array('2', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>B. ' . $row['odp2'] . '</p>
+                                            <input type="checkbox" name="odp[]" id="odp2" value="2"' . (in_array('2', $_SESSION['odp']) ? ' checked' : '') . '><p>B. ' . $row['odp2'] . '</p>
                                         </div>
                                         <div id="input">
-                                            <input type="checkbox" name="odp[]" id="odp3" value="3"' . (in_array('3', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>C. ' . $row['odp3'] . '</p>
+                                            <input type="checkbox" name="odp[]" id="odp3" value="3"' . (in_array('3', $_SESSION['odp']) ? ' checked' : ''). '><p>C. ' . $row['odp3'] . '</p>
                                         </div>
                                         <div id="input">
-                                            <input type="checkbox" name="odp[]" id="odp4" value="4"' . (in_array('4', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>D. ' . $row['odp4'] . '</p>
+                                            <input type="checkbox" name="odp[]" id="odp4" value="4"' . (in_array('4', $_SESSION['odp']) ? ' checked' : '') . '><p>D. ' . $row['odp4'] . '</p>
                                         </div>
-                                      </div>';
+                                        
+                                      </div><div id="batony">
+                                            <div style="order:2">
+                                            <form action="egzamin.php" method="post">
+                                                <input type="submit" value="Zakończ test" id="submit" >
+                                            </form>
+                                            </div>
+                                            <div style="order:1">
+                                            <form action="tyl.php" method="post" id="tyl">
+                                                <input type="submit" value="Poprzednie pytanie" id="submit" >
+                                            </form>
+                                            </div>
+                                            <div style="order:3">
+                                            <input type="submit" value="Następne pytanie" id="submit">
+                                            </div>
+                                        </div></form>';
                         } else {
-                            $dane = '<div id="odpowiedzi">
-                                        <div id="input">
-                                            <input type="radio" name="odp" id="odp1" value="1"' . (in_array('1', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>A. ' . $row['odp1'] . '</p>
+                            $dane = '<form action="przod.php" method="post" id="main">
+                                        <div id="odpowiedzi">' . switch($_SESSION['odp'][$_SESSION['count']-1]){
+
+                                          }  
+                                        
+                                        '<div id="input">
+                                            <input type="radio" name="odp" id="odp1" value="1"'. (($_SESSION['odp'][$_SESSION['count']]=="1")? ' checked': '') . '><p>A. ' . $row['odp1'] . '</p>
                                         </div>
                                         <div id="input">
-                                            <input type="radio" name="odp" id="odp2" value="2"' . (in_array('2', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>B. ' . $row['odp2'] . '</p>
+                                            <input type="radio" name="odp" id="odp2" value="2"' . (in_array('2', $_SESSION['odp']) ? ' checked' : '') . '><p>B. ' . $row['odp2'] . '</p>
                                         </div>
                                         <div id="input">
-                                            <input type="radio" name="odp" id="odp3" value="3"' . (in_array('3', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>C. ' . $row['odp3'] . '</p>
+                                            <input type="radio" name="odp" id="odp3" value="3"' . (in_array('3', $_SESSION['odp']) ? ' checked' : '') . '><p>C. ' . $row['odp3'] . '</p>
                                         </div>
                                         <div id="input">
-                                            <input type="radio" name="odp" id="odp4" value="4"' . (in_array('4', $_SESSION['odp'][$_SESSION['count']]) ? ' checked' : '') . '><p>D. ' . $row['odp4'] . '</p>
+                                            <input type="radio" name="odp" id="odp4" value="4"' . (in_array('4', $_SESSION['odp']) ? ' checked' : '') . '><p>D. ' . $row['odp4'] . '</p>
+                                        </div>'
+                                        
+                                    </div>
+                                    <div id="batony">
+                                        <div style="order:2">
+                                        <form action="egzamin.php" method="post">
+                                            <input type="submit" value="Zakończ test" id="submit" >
+                                        </form>
                                         </div>
-                                    </div>';
+                                        <div style="order:1">
+                                        <form action="tyl.php" method="post" id="tyl">
+                                            <input type="submit" value="Poprzednie pytanie" id="submit" >
+                                        </form>
+                                        </div>
+                                        <div style="order:3">
+                                        <input type="submit" value="Następne pytanie" id="submit">
+                                        </div>
+                                    </div></form>';
                         }
                         array_push($odpowiedzi, $dane);
                     }
                     echo $pytanie[$_SESSION['count']];
                     echo $odpowiedzi[$_SESSION['count']];
+                    print_r($_SESSION['odp']);
+                    echo $_SESSION['odp']['0'];
                 ?>
-                <div id="batony">
+                <script>
+                //     $('#submit').click(function() {
+                //     var val1 = $('#odp1').val();
+                //     var val2 = $('#odp2').val();
+                //     var val3 = $('#odp3').val();
+                //     var val4 = $('#odp4').val();
+                //     $.ajax({
+                //         type: 'POST',
+                //         url: 'egzamin.php',
+                //         data: { odp1: val1, odp2: val2, odp3: val3, odp4: val4 },
+                //         success: function(response) {
+                //             $('#result').html(response);
+                //         }
+                //     });
+                // });
+                </script>
+                <!-- <div id="batony">
                     <div style="order:2">
                     <form action="egzamin.php" method="post">
                         <input type="submit" value="Zakończ test" id="submit" >
@@ -103,7 +161,7 @@
                         <input type="submit" value="Następne pytanie" id="submit">
                     </form> 
                     </div>
-                </div>
+                </div> -->
             </form>
         </div>
     </div>
