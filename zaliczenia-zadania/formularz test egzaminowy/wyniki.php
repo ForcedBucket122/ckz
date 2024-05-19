393832228684398c278a1219ce913b863f2f40b7
ForcedBucket122 <151549270+ForcedBucket122@users.noreply.github.com>
2024-05-19 17:17:06 +0200

    Update wyniki.php

--- zaliczenia-zadania/formularz test egzaminowy/wyniki.php
+++ zaliczenia-zadania/formularz test egzaminowy/wyniki.php
@@ -1,4 +1,5 @@
 <?php
+    error_reporting(0);
     session_start();
     if (!isset($_SESSION['count'])) { 
         $_SESSION['count'] = 0;       
@@ -19,7 +20,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>egzamin</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="wynikcss.css">
 </head>
 <body>
     <div id="kontener">
@@ -54,6 +55,7 @@
                     }
                     $prawidlowe=array_fill(0, 10, "");
                     $bledne=array_fill(0, 10, "");
+                    $check=0;
                     for($i=0;$i<=9;$i++){
                         if($i!=9){
                             if(@$_SESSION['odp'][$i]==$popr_odp[$i]){
@@ -64,39 +66,46 @@
                             }
                         }else{
                             $x=0;
+                            
                             foreach(@$_SESSION['odp'][$i] as $odp){
                                 if(isset($odp)){
                                     if(($odp==$popr_odp[$i][0])or($odp==$popr_odp[$i][1])){
                                         $ok[$x]=$odp;
-                                        $pkt+=0.5;
+                                        $check+=0.5;
                                     }else{
                                         $zle[$x]=$odp;
-                                        $pkt+=-0.5;
                                     }
+                                    
                                     $prawidlowe[$i]=$ok;
                                     $bledne[$i]=$zle;
                                     $x++;
                                 }
                             }
                         }
+                        foreach(@$bledne[9] as $x){
+                            if($x!=null){
+                                $check=0;
+                            }
+                        }
+                        $pkt+=$check;
                     }
                     $procent=($pkt/10)*100;
                     while ($row = mysqli_fetch_array($query)) {
                         $dane = '<div id="pytanie">' . $nr . ". " . $row['treść_pyt'] . '</div>';
                         array_push($pytanie, $dane);
                     }
-                    echo $procent."%<br>";
-                    echo $pkt."/10";
+                    echo "<div id='procent'><span>".$procent."%</span></div><hr class='hr1'><br>";
+                    echo "<div id='pkt'>".$pkt."/10</div>";
                     while ($row = mysqli_fetch_array($query1)) {
                         if ($typ[$row['id_pyt'] - 1] > 1) {
                             $dane='';
                             $dane .= 
                                 '<form action="sprawdz.php" method="post" id="main">
                                     <div id="odpowiedzi">
-                                        <p'.@(($prawidlowe[$_SESSION['count']][0]==$row['odp1']) ? ' style="color: green;"' : '').@(($bledne[$_SESSION['count']][0]==$row['odp1']) ? ' style="color: red;"'    : '').'>A. ' . $row['odp1'] . '</p>
-                                        <p'.@(($prawidlowe[$_SESSION['count']][1]==$row['odp2']) ? ' style="color: green;"' : '').@(($bledne[$_SESSION['count']][1]==$row['odp2']) ? ' style="color: red;"'    : '').'>B. ' . $row['odp2'] . '</p>
-                                        <p'.@(($prawidlowe[$_SESSION['count']][2]==$row['odp3']) ? ' style="color: green;"' : '').@(($bledne[$_SESSION['count']][2]==$row['odp3']) ? ' style="color: red;"'    : '').'>C. ' . $row['odp3'] . '</p>
-                                        <p'.@(($prawidlowe[$_SESSION['count']][3]==$row['odp4']) ? ' style="color: green;"' : '').@(($bledne[$_SESSION['count']][3]==$row['odp4']) ? ' style="color: red;"'    : '').'>D. ' . $row['odp4'] . '</p>
+                                        <p'.@(($prawidlowe[$_SESSION['count']][0]==$row['odp1']) ? ' style="color: #02ff5b;"' : '').@(($bledne[$_SESSION['count']][0]==$row['odp1']) ? ' style="color: #ff0000;"'    : '').'>A. ' . $row['odp1'] . '</p>
+                                        <p'.@(($prawidlowe[$_SESSION['count']][1]==$row['odp2']) ? ' style="color: #02ff5b;"' : '').@(($bledne[$_SESSION['count']][1]==$row['odp2']) ? ' style="color: #ff0000;"'    : '').'>B. ' . $row['odp2'] . '</p>
+                                        <p'.@(($prawidlowe[$_SESSION['count']][2]==$row['odp3']) ? ' style="color:#02ff5b;"' : '').@(($bledne[$_SESSION['count']][2]==$row['odp3']) ? ' style="color: #ff0000;"'    : '').'>C. ' . $row['odp3'] . '</p>
+                                        <p'.@(($prawidlowe[$_SESSION['count']][3]==$row['odp4']) ? ' style="color: #02ff5b;"' : '').@(($bledne[$_SESSION['count']][3]==$row['odp4']) ? ' style="color: #ff0000;"'    : '').'>D. ' . $row['odp4'] . '</p>
                                     </div>
                                     <div id="batony">
                                         <div style="order:2">
@@ -115,28 +124,28 @@
                             $dane .= 
                                 '<form action="sprawdz.php" method="post" id="main">
                                     <div id="odpowiedzi">
-                                        <p'.@(($prawidlowe[$_SESSION['count']]==$row['odp1']) ? ' style="color: green;"' : '').(($bledne[$_SESSION['count']]==$row['odp1']) ? ' style="color: red;"'    : '').'>A. ' . $row['odp1'] . '</p>
-                                        <p'.@(($prawidlowe[$_SESSION['count']]==$row['odp2']) ? ' style="color: green;"' : '').(($bledne[$_SESSION['count']]==$row['odp2']) ? ' style="color: red;"'    : '').'>B. ' . $row['odp2'] . '</p>
-                                        <p'.@(($prawidlowe[$_SESSION['count']]==$row['odp3']) ? ' style="color: green;"' : '').(($bledne[$_SESSION['count']]==$row['odp3']) ? ' style="color: red;"'    : '').'>C. ' . $row['odp3'] . '</p>
-                                        <p'.@(($prawidlowe[$_SESSION['count']]==$row['odp4']) ? ' style="color: green;"' : '').(($bledne[$_SESSION['count']]==$row['odp4']) ? ' style="color: red;"'    : '').'>D. ' . $row['odp4'] . '</p>
+                                        <p'.@(($prawidlowe[$_SESSION['count']]==$row['odp1']) ? ' style="color: #02ff5b;"' : '').(($bledne[$_SESSION['count']]==$row['odp1']) ? ' style="color: #ff0000;"'    : '').'>A. ' . $row['odp1'] . '</p>
+                                        <p'.@(($prawidlowe[$_SESSION['count']]==$row['odp2']) ? ' style="color: #02ff5b;"' : '').(($bledne[$_SESSION['count']]==$row['odp2']) ? ' style="color: #ff0000;"'    : '').'>B. ' . $row['odp2'] . '</p>
+                                        <p'.@(($prawidlowe[$_SESSION['count']]==$row['odp3']) ? ' style="color: #02ff5b;"' : '').(($bledne[$_SESSION['count']]==$row['odp3']) ? ' style="color: #ff0000;"'    : '').'>C. ' . $row['odp3'] . '</p>
+                                        <p'.@(($prawidlowe[$_SESSION['count']]==$row['odp4']) ? ' style="color: #02ff5b;"' : '').(($bledne[$_SESSION['count']]==$row['odp4']) ? ' style="color: #ff0000;"'    : '').'>D. ' . $row['odp4'] . '</p>
                                     </div>
                                     <div id="batony">
                                         <div style="order:2">
-                                            <input type="submit" name="przycisk" value="Spróbuj ponownie" id="submit" >
+                                            <input type="submit" name="przycisk" value="Spróbuj ponownie" id="submit" class="pop" >
                                         </div>
                                         <div style="order:1">
                                             <input type="submit" name="przycisk" value="Poprzednie pytanie " id="submit" >
                                         </div>
                                         <div style="order:3">
-                                            <input type="submit" name="przycisk" value="Następne pytanie " id="submit">
+                                            <input type="submit" name="przycisk" value="Następne pytanie " id="submit" class="nast">
                                         </div>
                                     </div>
                                 </form>';
                         }
                         array_push($odpowiedzi, $dane);
                     }
-                    echo $pytanie[$_SESSION['count']];
-                    echo $odpowiedzi[$_SESSION['count']];
+                    echo "<div id='pytania'>".$pytanie[$_SESSION['count']];
+                    echo "<div id='odp'>".$odpowiedzi[$_SESSION['count']]."</div></div>";
                     // print_r($prawidlowe);
                     // print_r($bledne);
                 ?>
