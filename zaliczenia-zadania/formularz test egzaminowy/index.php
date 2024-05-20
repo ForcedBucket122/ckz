c9aca9dcbcb48142d4aeb8786f052a78b79d7167
ForcedBucket122 <dzejkob2112@gmail.com>
2024-05-20 19:31:26 +0200

    koniec

--- zaliczenia-zadania/formularz test egzaminowy/index.php
+++ zaliczenia-zadania/formularz test egzaminowy/index.php
@@ -8,27 +8,35 @@
 </head>
 <body>
     <div id="kontener">
-    <?php
-    @$klasa=$_POST['klasa'];
-    @$nazwisko=$_POST['nazwisko'];
-    @$imie=$_POST['imie'];
-    $con=mysqli_connect('localhost','root','','formularz');
-    $sprawdz='SELECT klasa, nazwisko, imię FROM użytkownicy;';
-    $query=mysqli_query($con,$sprawdz);
-    $wynik=0;
-    while($row=mysqli_fetch_array($query)){
-        if($klasa==$row[0]){
-            if($nazwisko==$row[1]){
-                if($imie==$row[2]){
+        <?php
+            @$klasa=$_POST['klasa'];
+            @$nazwisko=$_POST['nazwisko'];
+            @$imie=$_POST['imie'];
+            $con=mysqli_connect('localhost','root','','formularz');
+            $sprawdz='SELECT klasa, nazwisko, imię FROM użytkownicy;';
+            $query=mysqli_query($con,$sprawdz);
+            $wynik=0;
+            $klasa_tab=[];
+            $nazwisko_tab=[];
+            $imie_tab=[];
+            while($row=mysqli_fetch_array($query)){
+                array_push($klasa_tab, $row[0]);
+                array_push($nazwisko_tab, $row[1]);
+                array_push($imie_tab, $row[2]);
+            }
+            if(@$_POST['przycisk']=='zaloguj'){
+                if(in_array($klasa,$klasa_tab) and in_array($nazwisko,$nazwisko_tab) and in_array($imie,$imie_tab)){
                     header('location: egzamin.php');
                 }else{
-                    echo "<script type='text/javascript'>alert('Taki użytkownik nie istnieje');</script>";
+                    if((!isset($klasa) or $klasa==null) or (!isset($nazwisko) or $nazwisko==null) or (!isset($imie) or $imie==null)){
+                        echo "<script type='text/javascript'>alert('Wypełnij wszystkie pola!');</script>";
+                    }else{
+                        echo "<script type='text/javascript'>alert('Taki użytkownik nie istnieje!');</script>";
+                    }
                 }
             }
-        }
-    }
             mysqli_close($con);
-        ?>
+            ?>
         <hr>
         <div id="tak">
         <h1>Zaloguj</h1>
@@ -39,7 +47,7 @@
             <input type="text" name="nazwisko" id="nazwisko"> <br>
             Imie: <br>
             <input type="text" name="imie" id="imie"> <br> <br>
-            <input type="submit" value="zaloguj">
+            <input type="submit" value="zaloguj" name="przycisk">
         </form>
         </div>
     </div>
