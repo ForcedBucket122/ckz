f4d58fbd0698cc74a522a5ebb1df64cd0a04ab34
tomatotownmasacre <jakub7928@gmail.com>
2024-09-24 15:13:47 +0200

    zaliczenie

---
+++ 3/zaliczenie 1/zadania Java/src/Main.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //zad 1
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Podaj swoje imie: ");
+//        String imie = scanner.next();
+//        System.out.print("Podaj swoje nazwisko: ");
+//        String nazwisko = scanner.next();
+//        System.out.print("Wiaj "+imie+" "+nazwisko+". Pozdrawiam");
+//
+//        System.out.print("\nPodaj rok swojego urodzenia: ");
+//        int rok = scanner.nextInt();
+//        int wiek=2024-rok;
+//        System.out.println("Masz "+wiek+" lat");
+
+        //zad2
+//        for (int i = 0; i<10;i++){
+//            System.out.print(i+""+i);
+//        }
+
+        //zad3
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Podaj ocenę semestralną z geografii: ");
+//        int geo = scanner.nextInt();
+//        System.out.print("Podaj ocenę semestralną z biologii: ");
+//        int bio = scanner.nextInt();
+//        System.out.print("Podaj ocenę semestralną z historii: ");
+//        int hist = scanner.nextInt();
+//        System.out.print("Podaj ocenę semestralną z informatyki: ");
+//        int inf = scanner.nextInt();
+//
+//        int srednia = (geo+bio+hist+inf)/4;
+//        switch (srednia){
+//            case 1:
+//                System.out.println("jeden");
+//                break;
+//            case 2:
+//                System.out.println("dwa");
+//                break;
+//            case 3:
+//                System.out.println("trzy");
+//                break;
+//            case 4:
+//                System.out.println("cztery");
+//                break;
+//            case 5:
+//                System.out.println("pięć");
+//                break;
+//            case 6:
+//                System.out.println("sześć");
+//                break;
+//
+//            default:
+//                System.out.println("Błąd");
+//        }
+
+        //zad4
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("podaj a: ");
+//        int a = scanner.nextInt();
+//        System.out.print("podaj b: ");
+//        int b = scanner.nextInt();
+//
+//        int suma = a+b;
+//        if (suma%2==0){
+//            System.out.println("\nParzysta");
+//        }else {
+//            System.out.println("\nNieparzysta");
+//        }
+
+    }
+}
---
+++ 3/zaliczenie 1/zadania c#/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace zadania_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zad 1
+            //int[] tablica = new int[4];
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Console.Write("Podaj liczbe: ");
+            //    int liczba=int.Parse(Console.ReadLine());
+            //    tablica[i] = liczba;
+            //}
+            //for (int i = tablica.Length-1;i > -1; i--)
+            //{
+            //    Console.WriteLine(tablica[i]);
+            //}
+
+            //zad 2
+            //Console.Write("Podaj swoj wiek: ");
+            //int wiek = int.Parse(Console.ReadLine());
+            //if (wiek >0 && wiek <= 120)
+            //{
+            //    if (wiek >= 18)
+            //    {
+            //        Console.WriteLine("Jestes osobą pełnoletnią");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Przykro mi, ale nie jesteś osobą pełnoletnią");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Błąd!");
+            //}
+
+            //zad 3
+            //Console.Write("Podaj liczbę: ");
+            //int liczba = int.Parse(Console.ReadLine());
+            //int wynik = 0;
+            //if (liczba < 0)
+            //{
+            //    Console.WriteLine("Błąd!");
+            //}
+            //else
+            //{
+            //    wynik = (int)Math.Sqrt(liczba);
+            //    Console.WriteLine(wynik);
+            //}
+
+            //zad 4
+            //int suma = 0;
+            //int a = 1;
+            //while (a != 0)
+            //{
+            //    Console.Write("Podaj liczbe: ");
+            //    a= int.Parse(Console.ReadLine());
+            //    suma += a;
+            //}
+            //Console.WriteLine(suma);
+        }
+    }
+}
