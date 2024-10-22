3acb1388fb70b400c25256baf732c9c1ab402d31
tomatotownmasacre <jakub7928@gmail.com>
2024-10-22 16:35:37 +0200

    WPF ale poziom wyzej

---
+++ 3/WPF/zestaw2/Zad1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zad1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void potwierdz(object sender, RoutedEventArgs e)
+        {
+            int liczba;
+            String Symbol = symbol.Text;
+            String Nazwa = nazwa.Text;
+            String Magazyn = magazyn.Text;
+
+            bool czyMozna = Int32.TryParse(liczbaSztuk.Text, out liczba);
+            if (czyMozna)
+            {
+                MessageBox.Show("Wprowadzono dane: \n" + Symbol + " " + Nazwa + " " + liczba + " " + Magazyn);
+                liczbaSztuk.BorderBrush=System.Windows.Media.Brushes.Green;
+            }
+            else
+            {
+                liczbaSztuk.BorderBrush = System.Windows.Media.Brushes.Red;
+            }
+        }
+    }
+}
---
+++ 3/WPF/zestaw2/zad7/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zad7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void menuPlikOtworzPlik(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Filter = "Pliki tekstowe (.txt)|*.txt";
+
+            bool? result= dialog.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dialog.FileName;
+                File.Open(filename,FileMode.Open);
+            }
+        }
+    }
+}
