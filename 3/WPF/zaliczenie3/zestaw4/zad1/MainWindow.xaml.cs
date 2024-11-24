a0e43af2518b4bdac8285b01f550a2daca1fcb23
ForcedBucket122 <dzejkob2112@gmail.com>
2024-11-24 21:15:00 +0100

    zadanie 1

---
+++ 3/WPF/zaliczenie3/zestaw4/zad1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+
+namespace zad1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneratePasswordButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Pobierz długość hasła
+            if (!int.TryParse(PasswordLengthTextBox.Text, out int length) || length <= 0)
+            {
+                MessageBox.Show("Podaj poprawną długość hasła (liczba większa od 0).", "Błąd", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            // Sprawdź opcje
+            string characters = "";
+            if (IncludeLettersCheckBox.IsChecked == true)
+                characters += "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            if (IncludeDigitsCheckBox.IsChecked == true)
+                characters += "0123456789";
+            if (IncludeSpecialCharsCheckBox.IsChecked == true)
+                characters += "!@#$%^&*()-_=+[]{};:,.<>?";
+
+            if (string.IsNullOrEmpty(characters))
+            {
+                MessageBox.Show("Wybierz przynajmniej jedną opcję znaków.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            // Generuj hasło
+            Random random = new Random();
+            string password = "";
+            for (int i = 0; i < length; i++)
+            {
+                password += characters[random.Next(characters.Length)];
+            }
+
+            // Wyświetl wynik
+            MessageBox.Show($"Wygenerowane hasło: {password}", "Hasło", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
