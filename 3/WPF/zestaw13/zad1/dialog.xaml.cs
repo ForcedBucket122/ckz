a98612389d0fe04c1db222bf5e6659bbb4905665
tomatotownmasacre <jakub7928@gmail.com>
2025-04-01 16:45:48 +0200

    okna modalne

---
+++ 3/WPF/zestaw13/zad1/dialog.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace zad1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy dialog.xaml
+    /// </summary>
+    public partial class dialog : Window
+    {
+        public string ReturnValue { get; set; }
+        public dialog()
+        {
+            InitializeComponent();
+            this.Closing += Dialog_Closing;
+        }
+
+        private void Dialog_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            MessageBox.Show("Podaj imie!");
+        }
+
+        public void gotowe(object sender, RoutedEventArgs e) 
+        {
+            String imie = imieIn.Text;
+            if(imie.Length>2)
+            {
+                ReturnValue = imie;
+                this.Closing -= Dialog_Closing;
+            }
+            else
+            {
+                MessageBox.Show("Imie musi mieć conajmniej 3 litery!");
+            }
+        }
+    }
+}
---
+++ 3/WPF/zestaw13/zad4/okno2.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace zad4
+{
+    /// <summary>
+    /// Logika interakcji dla klasy okno2.xaml
+    /// </summary>
+    public partial class okno2 : Window
+    {
+        public okno2()
+        {
+            InitializeComponent();
+        }
+        public void NumberValidation(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled=!IsTextAllowed(e.Text);
+        }
+        private static bool IsTextAllowed(string text)
+        {
+            return int.TryParse(text, out _); // Sprawdza, czy tekst można przekonwertować na liczbę
+        }
+        public void Ok(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Name: {Name.Text}\nEmployment date: {Date.Text}\nDepartment: {Department.Text}\nPosition: {Postion.Text}\nAnnual salary: ${Salary.Text}\nJob description: {Description.Text}");
+            this.Close();
+        }
+        public void Cancel(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        
+    }
+}
