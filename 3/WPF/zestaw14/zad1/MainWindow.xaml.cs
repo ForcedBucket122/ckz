357e492b5d0d35dd73c2d45692277f2fe065ae42
ForcedBucket122 <dzejkob2112@gmail.com>
2025-04-08 16:30:12 +0200

    zadanie 1 z nowego zestawu

---
+++ 3/WPF/zestaw14/zad1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zad1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void Otworz(object sender, RoutedEventArgs e)
+        {
+            var selected = lista.SelectedItem as ListBoxItem;
+            if (selected != null)
+            {
+                String link = (string)selected.Content;
+                Process.Start(new ProcessStartInfo("https://" + link) { UseShellExecute = true });
+            }
+            else
+            {
+                MessageBox.Show("Zaznacz pole! ", "błąd",MessageBoxButton.OK ,MessageBoxImage.Warning);
+            }
+
+                //MessageBox.Show(link); 
+
+                
+        }
+    }
+}
